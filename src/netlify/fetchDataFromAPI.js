74a87061c00278ed02185f007a409589927a096b
Ikraam <IbnDaanis@Gmail.com>
2021-01-25 15:59:01 +0000

    Added dev and prod apis

---
+++ src/netlify/fetchDataFromAPI.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+require('dotenv').config()
+
+const API_ENDPOINT = process.env.PRODUCTION_API
+
+exports.handler = async function () {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ response }),
+  }
+}
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,16 @@
 const getDataFromAPI = async () => {
+  console.log(process.env.API_KEY)
+  console.log(process.env.PRODUCTION)
+  console.log(process.env.PRODUCTION_API)
+  console.log(process.env.DEVELOPMENT_API)
   try {
-    const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.API_KEY}`
+    const apiURL =
+      process.env.PRODUCTION === 'true'
+        ? process.env.PRODUCTION_API
+        : process.env.DEVELOPMENT_API
     const fetchData = await fetch(apiURL)
     const response = await fetchData.json()
-    console.log(process.env.API_KEY)
-    console.log(process.env.PRODUCTION)
+
     console.log(response)
   } catch (error) {
     console.error(error)

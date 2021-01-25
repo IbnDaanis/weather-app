a91c0b15790121680783f0fbabc5437842451f95
Ikraam <IbnDaanis@Gmail.com>
2021-01-25 17:09:29 +0000

    Added search input

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,14 +3,24 @@ const getDataFromAPI = async searchQuery => {
     const apiURL =
       process.env.PRODUCTION === 'true'
         ? `/.netlify/functions/fetchDataFromAPI?search=${searchQuery}`
-        : `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${process.env.API_KEY}`
+        : `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&units=metric&appid=${process.env.API_KEY}`
     const fetchData = await fetch(apiURL)
     const response = await fetchData.json()
 
     console.log(response)
+    const cityEl = document.querySelector('.city')
+    const tempEl = document.querySelector('.temp')
+    cityEl.textContent = response.name
+    tempEl.textContent = `${Math.round(response.main.temp)}°C`
   } catch (error) {
     console.error(error)
   }
 }
-const searchQuery = 'New York'
-getDataFromAPI(searchQuery)
+const searchWeatherForm = document.querySelector('#searchWeatherForm')
+const searchInput = searchWeatherForm.querySelector('input')
+
+searchWeatherForm.onsubmit = e => {
+  e.preventDefault()
+  getDataFromAPI(searchInput.value)
+  searchInput.value = ''
+}
